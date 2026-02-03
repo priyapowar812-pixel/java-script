84ac2ac39c29026ec99432e663747ebbd2cc0ab0
priyapowar812-pixel <priyapowar812@gmail.com>
2026-02-03 17:02:08 +0530

    constructor code in js

--- constructor.js
+++ constructor.js
@@ -4,11 +4,43 @@ class Student{
        this.name = name;
        this.age = age;
        this.batch = [this.name];
+
+       
     }
-    admission() {
+
+    // setting and getting name property..
+    set name(newName){
+         
+         if(newName.length >0 && typeof newName === "string"){
+          this._name = newName;
+        }else{
+        console.error("name should be nonempty string");
+        }
+    }
+
+    get name(){
+        return this._name;
+    }
+
+    // setting and getting age property
+
+    set age(newAge){
+       if(typeof newAge === "number" && newAge > 0){
+        this._age = newAge;
+       }
+       else{
+        console.error("enter valid age");
+       }
+    }
+    get age(){
+      return this._age;
+    }
+
+    admission(){
         console.log(`${this.name} has taken admission`);
     }
 }
+
 
 class ArtStudent extends Student{
    constructor(name,age,subject){
@@ -44,9 +76,9 @@ class CommerceStudent extends Student{
    }
 }
 
-const artStudent = new ArtStudent("saraswati", 30 , "history");
+const artStudent = new ArtStudent("saraswati", -24 , "history");
 const comStudent = new CommerceStudent("maya", 30 , "maths");
 const scienceStudent = new ScienceStudent("saraswati", 30 , "science");
 
-console.log(artStudent);
+console.log(artStudent.age);
 console.log(artStudent.admissionStream());
